1b82461c86915860e42b22239a5190dbbedd4d75
Aaron Schlesinger <aschlesinger@paypal.com>
2014-06-09 10:20:43 -0700

    converting triggerUserEventHandler to use the server framework

--- user_event_handlers.go
+++ user_event_handlers.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
 	"net/http"
-	"strconv"
+	"github.com/arschles/serf-proxy/server"
 )
 
 type userEventRequestPayload struct {
@@ -13,33 +11,26 @@ type userEventRequestPayload struct {
 }
 
 func (baseHandler *BaseHandler) triggerUserEventHandler(resp http.ResponseWriter, req *http.Request) {
-	coalesceStr, err := queryString(req, "coalesce", 0)
-	if err != nil {
-		http.Error(resp, "missing coalesce flag", http.StatusBadRequest)
-		return
+	var coalesce bool
+	var payload userEventRequestPayload
+	steps := []*server.HttpStep {
+		&server.HttpStep {
+			Runner: server.QueryStringParseBool(req, "coalesce", 0, &coalesce),
+			FailCode: http.StatusBadRequest,
+			FailMsg: server.JsonErr(),
+		},
+		&server.HttpStep {
+			Runner: server.ReadJson(req, &payload),
+			FailCode: http.StatusBadRequest,
+			FailMsg: server.JsonErr(),
+		},
+		&server.HttpStep {
+			Runner: func() error {
+				return baseHandler.client.UserEvent(payload.name, payload.payload, coalesce)
+			},
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
 	}
-	coalesce, err := strconv.ParseBool(coalesceStr)
-	if err != nil {
-		http.Error(resp, "invalid coalesce flag", http.StatusBadRequest)
-		return
-	}
-
-	body, readAllErr := ioutil.ReadAll(req.Body)
-	if readAllErr != nil {
-		http.Error(resp, readAllErr.Error(), http.StatusBadRequest)
-		return
-	}
-
-	payload := userEventRequestPayload{}
-	userEventRequestPayloadParseErr := json.Unmarshal(body, &payload)
-	if userEventRequestPayloadParseErr != nil {
-		http.Error(resp, userEventRequestPayloadParseErr.Error(), http.StatusInternalServerError)
-		return
-	}
-	userEventErr := baseHandler.client.UserEvent(payload.name, payload.payload, coalesce)
-	if userEventErr != nil {
-		http.Error(resp, userEventErr.Error(), http.StatusInternalServerError)
-		return
-	}
-	resp.WriteHeader(http.StatusNoContent)
+	server.NewFailHttpImmediately(resp, steps, http.StatusNoContent, server.EmptyBody()).Execute()
 }
