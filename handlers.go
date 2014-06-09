7092ca2dbc97e48ef1fc51d43921f2506a05e33c
Aaron Schlesinger <aschlesinger@paypal.com>
2014-06-09 10:34:53 -0700

    converting remaining handlers to use the server framework

--- handlers.go
+++ handlers.go
@@ -2,37 +2,90 @@ package main
 
 import (
 	"net/http"
+	"github.com/arschles/serf-proxy/server"
 )
 
 func (baseHandler *BaseHandler) keysHandler(resp http.ResponseWriter, req *http.Request) {
-	keys, total, _, err := baseHandler.client.ListKeys()
-	if err != nil {
-		writeJsonErr(http.StatusInternalServerError, err, resp)
-		return
+	var totalKeys map[string]int
+	var totalNumKeys int
+	var keys map[string]string
+	var bytes []byte
+	steps := []*server.HttpStep {
+		&server.HttpStep {
+			Runner: func() error {
+				totKeys, totNumKeys, k, kErr := baseHandler.client.ListKeys()
+				if kErr != nil {
+					return kErr
+				}
+				totalKeys = totKeys
+				totalNumKeys = totNumKeys
+				keys = k
+				return nil
+			},
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
+		&server.HttpStep {
+			Runner: server.EncodeJson(map[string]interface{}{"keys": keys}, &bytes),
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
 	}
-	jsonMap := map[string]interface{}{"keys": keys, "total": total}
-	writeJson(http.StatusOK, jsonMap, resp)
+	server.NewFailHttpImmediately(resp, steps, http.StatusOK, bytes).Execute()
 }
 
 func (baseHandler BaseHandler) statsHandler(resp http.ResponseWriter, req *http.Request) {
-	stats, err := baseHandler.client.Stats()
-	if err != nil {
-		writeJson(http.StatusInternalServerError, err.Error, resp)
-		return
+	var stats map[string]map[string]string
+	var bytes []byte
+	steps := []*server.HttpStep {
+		&server.HttpStep {
+			Runner: func() error {
+				s, err := baseHandler.client.Stats()
+				if err != nil {
+					return err
+				}
+				stats = s
+				return nil
+			},
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
+		&server.HttpStep {
+			Runner: server.EncodeJson(stats, &bytes),
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
 	}
-	writeJson(http.StatusOK, stats, resp)
+	server.NewFailHttpImmediately(resp, steps, http.StatusOK, bytes).Execute()
 }
 
 func (baseHandler *BaseHandler) useKeyHandler(resp http.ResponseWriter, req *http.Request) {
-	key, err := queryString(req, "key", 0)
-	if err != nil {
-		writeJson(http.StatusBadRequest, "invalid key", resp)
-		return
+	var key string
+	var keyRing map[string]string
+	var bytes []byte
+	steps := []*server.HttpStep {
+		&server.HttpStep {
+			Runner: server.QueryString(req, "key", 0, &key),
+			FailCode: http.StatusBadRequest,
+			FailMsg: server.JsonErr(),
+		},
+		&server.HttpStep {
+			Runner: func() error {
+				k, err := baseHandler.client.UseKey(key)
+				if err != nil {
+					return err
+				}
+				keyRing = k
+				return nil
+			},
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
+		&server.HttpStep {
+			Runner: server.EncodeJson(keyRing, &bytes),
+			FailCode: http.StatusInternalServerError,
+			FailMsg: server.JsonErr(),
+		},
 	}
-	keyRing, err := baseHandler.client.UseKey(key)
-	if err != nil {
-		writeJsonErr(http.StatusInternalServerError, err, resp)
-		return
-	}
-	writeJson(http.StatusOK, keyRing, resp)
+	server.NewFailHttpImmediately(resp, steps, http.StatusOK, bytes).Execute()
 }
